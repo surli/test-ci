27009d6cab02a8c622b009db10030121f8a6d58a
Simon Urli <simon.urli@inria.fr>
2017-10-12 17:04:21 +0200

    Init repository

---
+++ src/main/java/DumbClass.java
@@ -0,0 +1,6 @@
+/**
+ * Created by urli on 12/10/2017.
+ */
+public class DumbClass {
+    public static int FOO = 42;
+}
---
+++ src/test/java/TestDumbClass.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by urli on 12/10/2017.
+ */
+public class TestDumbClass {
+
+    @Test
+    public void testFoo() {
+        assertEquals(42, DumbClass.FOO);
+    }
+}
